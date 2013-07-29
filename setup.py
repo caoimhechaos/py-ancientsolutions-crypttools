742f4dc7b34ecc7348b1d79d79bd7a5dbacc3315
Tonnerre LOMBARD <tonnerre@ancient-solutions.com>
2013-07-29 23:53:50 +0200

    Rename to ancientsolutions-crypttools.

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='crypttools',
+setup(name='ancientsolutions-crypttools',
       version='0.1',
       description='Python Cryptography Utilities',
       author='Tonnerre LOMBARD',
       author_email='tonnerre@ancient-solutions.com',
-      url='http://python.ancient-solutions.com/py-crypttools/',
-      packages=['ancientsolutions.crypttools.RSAKeyUnwrapper'],
+      url='http://python.ancient-solutions.com/crypttools/',
+      package_dir = {'': 'lib'},
+      packages=['ancientsolutions.crypttools'],
      )
