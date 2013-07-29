005d0af7700a33824e00705017697f0a5ce1cbe8
Caoimhe Chaos <caoimhechaos@protonmail.com>
2013-07-29 23:47:24 +0200

    Add some Python classes for figuring out how things work. Also, they're helpful.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='crypttools',
+      version='0.1',
+      description='Python Cryptography Utilities',
+      author='Caoimhe Chaos',
+      author_email='caoimhechaos@protonmail.com',
+      url='http://python.ancient-solutions.com/py-crypttools/',
+      packages=['ancientsolutions.crypttools.RSAKeyUnwrapper'],
+     )
